a70f2e2f0c282ce977cea3a921c587df0f05dbb0
charlesLoder <charles.loder@northwestern.edu>
2025-01-21 16:40:04 -0500

    Add api url endpoint

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
 export default function Home() {
+  const apiUrl = process.env.API_URL;
+
+  if (!apiUrl) {
+    throw new Error("API_URL is not defined");
+  }
+
   return (
+    <>
     <h1>Hello, cruel world!</h1>
+    {/* add a button click that fetches from the apiURL */}
+    <button onClick={async () => {
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+      console.log(data);
+    }}>Fetch from API</button>
+    </>
   );
 }
